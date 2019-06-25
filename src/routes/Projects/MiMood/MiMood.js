518c1ae62d24f6a82e4eaa48ed3998c36443cafb
Loxphordex <silasishallahan@gmail.com>
2019-06-25 14:29:09 -0700

    mimood basic

---
+++ src/routes/Projects/MiMood/MiMood.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import happy from '../../../images/happy.png'
+import miMoodMobile from '../../../images/miMood-newEntry.png'
+import './MiMood.css'
+
+export default class MiMood extends React.Component {
+  render() {
+    return(
+    <div id='miMood' className='project-wrapper collapsed'
+        onClick={(event) => this.props.toggleProjectInfo(event)}>
+
+        <div className='project miMood'>
+        <p>MIMOOD</p>
+        </div>
+
+        <div className='project-info'>
+        <div className='info-background'>
+            <img src={happy} alt='miMood' className='miMood-image' />
+            <div  className='vl vl3' />
+            <img src={miMoodMobile} alt='miMood-mobile' className='miMood-mobile' />
+            <div  className='vl vl4' />
+
+            <div className='project-links miMood-links'>
+                <a href='https://loxphordex-quoter-client.now.sh/' 
+                target='_blank' rel='noopener noreferrer'>Live App</a>
+                <a href='https://github.com/Loxphordex/silas-capstone-client' 
+                target='_blank' rel='noopener noreferrer'>GitHub</a>
+            </div>
+
+            <div className='info-container miMood-info'>
+              <h2 className='miMood-title'>miMood</h2>
+              <h2 className='miMood-purple'>journal</h2>
+              <div className='hl hl1' />
+            
+              <div className='p-container mm-p'>
+                  <p>Track your emotions using AI. 
+                    Users can submit written entries and selifes, and AI systems will
+                    analyze the input.</p>
+                  <p>This allows for a deeper understanding of both your own mood, 
+                    and of how AI interacts with human emotion.</p>
+            </div>
+
+            </div>
+            <div className = 'vl v2' />
+
+        </div>
+        </div>
+    </div>
+    )
+  }
+}
--- src/routes/Projects/Projects.js
+++ src/routes/Projects/Projects.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import quoterLabel from '../../images/quoter-label.png'
-import qouterMobile from '../../images/quoter-mobile.png'
 import './Projects.css'
 import './ProjectInfo.css'
+import Quoter from './Quoter/Quoter'
+import MiMood from './MiMood/MiMood'
 
 export default class Projects extends React.Component {
 
@@ -47,55 +46,9 @@ export default class Projects extends React.Component {
   
           <div className='projects-list'>
 
+            <Quoter toggleProjectInfo={this.toggleProjectInfo} />
 
-
-            <div id='quoter' className='project-wrapper collapsed' 
-              onClick={(event) => this.toggleProjectInfo(event)}>
-
-              <div className='project quoter'>
-                <p>QUOTER</p>
-              </div>
-
-              <div className='project-info'>
-                <div className='info-background'>
-                  <img src={quoterLabel} alt='quoter' className='quoter-project-image' />
-                  <img src={qouterMobile} alt='quoter-mobile' className='quoter-mobile' />
-                  <div  className='vl vl1' />
-
-                  <div className='info-container'>
-                    <h2 className='quoter-title'>Quoter</h2>
-                    <div className='hl hl1' />
-                    <p>A journal app that scans what you write, 
-                      and returns relevant quotes to spark inspiration.</p>
-                    <p>Discover new authors as well as new perspectives.</p>
-                    <div className='project-links quoter-links'>
-                      <a href='https://loxphordex-quoter-client.now.sh/' 
-                        target='_blank' rel='noopener noreferrer'>Live App</a>
-                      <a href='https://github.com/Loxphordex/silas-capstone-client' 
-                        target='_blank' rel='noopener noreferrer'>GitHub</a>
-                    </div>
-
-                  </div>
-                  <div className = 'vl v2' />
-
-                </div>
-              </div>
-            </div>
-
-
-            <div id='miMood' className='project-wrapper collapsed'
-              onClick={(event) => this.toggleProjectInfo(event)}>
-
-              <div className='project miMood'>
-                <p>MIMOOD</p>
-              </div>
-
-              <div className='project-info'>
-                <div className='info-background'>
-                  
-                </div>
-              </div>
-            </div>
+            <MiMood toggleProjectInfo={this.toggleProjectInfo} />
 
 
             <div id='bookmarks' className='project-wrapper collapsed'
