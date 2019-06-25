f79a32b7858ff5dbd9e2cbd9643a60521f0772a0
Loxphordex <silasishallahan@gmail.com>
2019-06-24 18:56:20 -0700

    quoter main stuff

--- src/routes/Projects/Projects.js
+++ src/routes/Projects/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import quoterLanding from '../../images/quoter-landing.png'
+import { Link } from 'react-router-dom'
+import quoterLabel from '../../images/quoter-label.png'
+import qouterMobile from '../../images/quoter-mobile.png'
 import './Projects.css'
+import './ProjectInfo.css'
 
 export default class Projects extends React.Component {
 
@@ -28,7 +31,7 @@ export default class Projects extends React.Component {
 
     if(isExpanded) {
       console.log('has')
-      element.setAttribute('class', `project-wrapper`)
+      element.setAttribute('class', `project-wrapper collapsed`)
       return;
     }
   }
@@ -46,7 +49,7 @@ export default class Projects extends React.Component {
 
 
 
-            <div id='quoter' className='project-wrapper' 
+            <div id='quoter' className='project-wrapper collapsed' 
               onClick={(event) => this.toggleProjectInfo(event)}>
 
               <div className='project quoter'>
@@ -55,13 +58,32 @@ export default class Projects extends React.Component {
 
               <div className='project-info'>
                 <div className='info-background'>
-                  
+                  <img src={quoterLabel} alt='quoter' className='quoter-project-image' />
+                  <img src={qouterMobile} alt='quoter-mobile' className='quoter-mobile' />
+                  <div  className='vl vl1' />
+
+                  <div className='info-container'>
+                    <h2 className='quoter-title'>Quoter</h2>
+                    <div className='hl hl1' />
+                    <p>A journal app that scans what you write, 
+                      and returns relevant quotes to spark inspiration.</p>
+                    <p>Discover new authors as well as new perspectives.</p>
+                    <div className='project-links quoter-links'>
+                      <a href='https://loxphordex-quoter-client.now.sh/' 
+                        target='_blank' rel='noopener noreferrer'>Live App</a>
+                      <a href='https://github.com/Loxphordex/silas-capstone-client' 
+                        target='_blank' rel='noopener noreferrer'>GitHub</a>
+                    </div>
+
+                  </div>
+                  <div className = 'vl v2' />
+
                 </div>
               </div>
             </div>
 
 
-            <div id='miMood' className='project-wrapper'
+            <div id='miMood' className='project-wrapper collapsed'
               onClick={(event) => this.toggleProjectInfo(event)}>
 
               <div className='project miMood'>
@@ -76,14 +98,14 @@ export default class Projects extends React.Component {
             </div>
 
 
-            <div id='bookmarks' className='project-wrapper'
+            <div id='bookmarks' className='project-wrapper collapsed'
               onClick={(event) => this.toggleProjectInfo(event)}>
 
               <div className='project bookmarks'>
                 <p>BOOKMARKS</p>
               </div>
 
-              <div className='project-info'>
+              <div className='project-info end-project'>
                 <div className='info-background'>
                   
                 </div>
