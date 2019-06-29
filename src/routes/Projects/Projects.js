c115b1b53c7ed25a113c6ef5aa458a72898e46fb
Loxphordex <silasishallahan@gmail.com>
2019-06-28 21:06:26 -0700

    consistent layout, state

--- src/routes/Projects/Projects.js
+++ src/routes/Projects/Projects.js
@@ -1,39 +1,22 @@
 import React from 'react'
+
+// STYLES
 import './Projects.css'
 import './ProjectInfo.css'
-import Quoter from './Quoter/Quoter'
-import MiMood from './MiMood/MiMood'
-import Bookmarks from './Bookmarks/Bookmarks'
+import '../../components/Project/Project.css'
+import './ProjectDetails.css'
+
+// IMAGES
+import quoterLabel from '../../images/quoter-label.png'
+import quoterMobile from '../../images/quoter-mobile.png'
+import happy from '../../images/happy.png'
+import newEntry from '../../images/miMood-newEntry.png'
+import bookmarksWide from '../../images/bookmarks-wide.png'
+import bookmarksMobile from '../../images/bookmarks-mobile.png'
+
+import Project from '../../components/Project/Project'
 
 export default class Projects extends React.Component {
-
-  componentDidMount() {
-    setTimeout(() => {
-      document.getElementById('quoter').setAttribute('class', 'project-wrapper expanded')
-    }, 50)
-  }
-
-  toggleProjectInfo(event) {
-
-
-    const element = document.getElementById(event.target.id)
-
-    if (!element) return;
-
-    const isExpanded = element.classList.contains('expanded')
-
-    // Here the class "expanded" is being toggled for each project
-    // This displays or colapses the info panel
-    if(!isExpanded) {
-      element.setAttribute('class', `project-wrapper expanded`)
-      return;
-    }
-
-    if(isExpanded) {
-      element.setAttribute('class', `project-wrapper collapsed`)
-      return;
-    }
-  }
 
   render() {
     return (
@@ -46,11 +29,63 @@ export default class Projects extends React.Component {
   
           <div className='projects-list'>
 
-            <Quoter toggleProjectInfo={this.toggleProjectInfo} />
+            <Project 
+              first={true}
+              name='quoter'
+              title='Quoter'
+              photo={quoterLabel}
+              mobilePhoto={quoterMobile}
+              frontEnd='Javascript, React'
+              backEnd='Express, PostgreSQL'
+              liveLink='https://loxphordex-quoter-client.now.sh/'
+              repoLink='https://github.com/Loxphordex/silas-capstone-client'
+              paragraph1='A journal app that scans what you write, 
+                and returns relevant quotes to spark inspiration.'
+              paragraph2='Discover new authors as well as new perspectives.'
+              last=''
+            />
+
+            <Project 
+              first={false}
+              name='miMood'
+              title='miMood'
+              photo={happy}
+              mobilePhoto={newEntry}
+              frontEnd='Javascript, React, Recharts'
+              backEnd='Express, PostgreSQL'
+              liveLink='https://alt-states-mimood.now.sh/'
+              repoLink='https://github.com/thinkful-ei-bee/Altered-States-Capstone'
+              paragraph1='Track your emotions using AI. 
+                Users can submit written entries and selifes, and AI systems will
+                analyze the input.'
+              paragraph2='This allows for a deeper understanding of both your own mood, 
+                and of how AI interacts with human emotion.'
+              last=''
+            />
+
+            <Project 
+              first={false}
+              name='bookmarks'
+              title='Bookmarks'
+              photo={bookmarksWide}
+              mobilePhoto={bookmarksMobile}
+              frontEnd='Javascript, jQuery'
+              backEnd=''
+              liveLink='https://loxphordex.github.io/Bookmarks/'
+              repoLink='https://github.com/Loxphordex/Bookmarks'
+              paragraph1='This was the first Javascript project I worked on. 
+                It was a collaberative, two-day assignment that drilled us 
+                on jQuery.'
+              paragraph2='The app simply keeps a record of your bookmarks, 
+                and lets you filter them by rating.'
+              last='last'
+            />
+
+            {/* <Quoter toggleProjectInfo={this.toggleProjectInfo} />
 
             <MiMood toggleProjectInfo={this.toggleProjectInfo} />
 
-            <Bookmarks toggleProjectInfo={this.toggleProjectInfo} />
+            <Bookmarks toggleProjectInfo={this.toggleProjectInfo} /> */}
           </div>
         </div>
     ) 
